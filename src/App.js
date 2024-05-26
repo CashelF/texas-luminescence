5e7f26ce0e5a2a331abf0b24c0d94f6b712c1771
Cashel <cashel@utexas.edu>
2024-05-26 00:20:14 -0500

    Initialize main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="nav">
+          <div className="logo">
+            <img src={`${process.env.PUBLIC_URL}/texas-luminescence-logo.jpeg`} alt="Texas Luminescence" />
+            <h1>Texas Luminescence</h1>
+          </div>
+          <div className="center-logo">
+          </div>
+          <ul>
+            <li><a href="#">Current Projects</a></li>
+            <li><a href="#">Recruitment</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Log in</a></li>
+            <li><button className="register-button">Register</button></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
